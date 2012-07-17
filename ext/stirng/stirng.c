8d513022dbb63dd16201badf5ba617eaeeac43c2
Corey Woodcox <corey.woodcox@gmail.com>
2012-07-17 14:13:35 -0600

    order matters in C. I forgot.

--- ext/stirng/stirng.c
+++ ext/stirng/stirng.c
@@ -1,12 +1,5 @@
 #include <ruby.h>
 
-void Init_stirng(void) {
-  VALUE module = rb_define_module("Stirng",
-      rb_cObject);
-
-  rb_define_module_function(module, "distance", stirng_distance, 2);
-}
-
 static VALUE stirng_distance(VALUE str1, VALUE str2) {
   str1 = rb_str_strip_bang(str1);
   str2 = rb_str_strip_bang(str2);
@@ -15,3 +8,10 @@ static VALUE stirng_distance(VALUE str1, VALUE str2) {
     return 1.0;
   }
 }
+
+void Init_stirng(void) {
+  VALUE module = rb_define_module("Stirng");
+
+  rb_define_module_function(module, "distance", stirng_distance, 2);
+}
+
